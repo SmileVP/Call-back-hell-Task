29b1476c8116affde44561c8676d53dfe48c875b
Vishnupriya S <108866164+SmileVP@users.noreply.github.com>
2022-09-26 11:42:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+ let div=document.createElement('div');
+ let span=document.createElement('span');
+ span.innerHTML="10";
+ div.append(span);
+ document.body.append(div);
+
+
+ 
+ setTimeout(()=>{ 
+ span.innerHTML="10";
+ setTimeout(()=>{
+ span.innerHTML="9";
+ setTimeout(()=>{
+ span.innerHTML="8";
+ setTimeout(()=>{
+ span.innerHTML="7";
+ setTimeout(()=>{
+ span.innerHTML="6";
+ setTimeout(()=>{
+ span.innerHTML="5";
+ setTimeout(()=>{
+ span.innerHTML="4";
+ setTimeout(()=>{
+ span.innerHTML="3";
+ setTimeout(()=>{
+ span.innerHTML="2";
+ setTimeout(()=>{
+ span.innerHTML="1";
+ setTimeout(()=>{
+ span.innerHTML="Happy Independence day";
+ },1000)
+ },1000)
+ },1000)
+ },1000)
+ },1000)
+ },1000)
+ },1000)
+ },1000)
+ },1000)
+ },1000)
+ },1000)
+
+ 
